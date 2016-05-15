ec7fb01be4bc6073c5c2748dea971f21be39545d
lllpla <lllpla@163.com>
2016-05-15 11:27:54 +0800

    build
    build

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeeMusic
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public playList pl = new LeeMusic.playList();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //播放音乐
+        private void btnPlay(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //前一首
+        private void btnBack(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //下一首
+        private void btnFoward(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //播放列表的显示和隐藏
+        private void readPlayList(object sender, RoutedEventArgs e)
+        {
+            if (pl.IsVisible)
+            {
+                pl.Hide();
+            }
+            else {
+                pl.Show();
+            }
+        }
+
+        private void closeWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ playList.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace LeeMusic
+{
+    /// <summary>
+    /// playList.xaml 的交互逻辑
+    /// </summary>
+    public partial class playList : Window
+    {
+        public playList()
+        {
+            InitializeComponent();
+        }
+        //扫描路径下的所有文件
+        private void openDIR_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+            //1.检查文件夹是否存在
+            if (textBox.Text.Equals(String.Empty)) {
+                return;
+            }
+            //2.得到所有文件名
+            DirectoryInfo info = new DirectoryInfo(textBox.Text);
+            //如果文件夹存在
+            
+            if (info.Exists) {
+                FileInfo[] files = info.GetFiles();
+                foreach (FileInfo file in files){
+                    listBox.Items.Add(file.FullName);
+                }
+
+            }
+
+        }
+
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.Equals("在此输入文件夹路径")) {
+                textBox.Text = "";
+            }
+        }
+
+        private void textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.Equals(String.Empty)) {
+                textBox.Text = "在此输入文件夹路径";
+            }
+        }
+    }
+}
